78648c57fd33d9e01106bd7630c4282ca445de65
Bim-Plugin-Team <zwe1992htet@gmail.com>
2025-05-03 01:31:55 +0800

    Added Wall Sandwich Button

---
+++ TT 1.0.extension/TT 1.0.tab/Pain Points.panel/Wall Sandwich.pushbutton/script.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+__title__   = "Wall Sandwich"
+__doc__     = """Version = 1.0
+Date    = 15.06.2024
+________________________________________________________________
+Description:
+
+
+________________________________________________________________
+Author: Zwe"""
+
+from pyrevit import forms
+from Autodesk.Revit.DB import *
+import datetime
+
+
+uidoc = __revit__.ActiveUIDocument
+doc = uidoc.Document
+
+forms.alert("🙄 Work in progress!!!Coming Soon")
+
+
